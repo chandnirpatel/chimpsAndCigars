7af38b863d796bb63dbe7f7de64ad5eca08506eb
Chandni Patel <chandni_patel@gap.com>
2017-02-02 22:33:29 -0800

    made test pass in easiest way possible

--- src/main/java/com/chandni/tdd/ChipsAndCigars.java
+++ src/main/java/com/chandni/tdd/ChipsAndCigars.java
@@ -3,6 +3,10 @@ package com.chandni.tdd;
 public class ChipsAndCigars {
 
     public boolean cigarPary(int numOfCigars, boolean isWeekend) {
+
+        if (numOfCigars >= 40) {
+            return true;
+        }
         return false;
     }
 }
