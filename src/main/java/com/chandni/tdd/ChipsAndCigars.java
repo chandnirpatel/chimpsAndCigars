ce6a1c3c91bbde8ff9be2b00c2c8376b80df033a
Chandni Patel <chandni_patel@gap.com>
2017-02-02 22:46:27 -0800

    refactored code

--- src/main/java/com/chandni/tdd/ChipsAndCigars.java
+++ src/main/java/com/chandni/tdd/ChipsAndCigars.java
@@ -3,8 +3,13 @@ package com.chandni.tdd;
 public class ChipsAndCigars {
 
     private int minCigarsForParty = 40;
+    private int maxCigarsForParty = 60;
 
     public boolean cigarPary(int numOfCigars, boolean isWeekend) {
-        return numOfCigars >= minCigarsForParty && numOfCigars <= 60 ? true : false;
+        return isAppropriateNumOfCigars(numOfCigars) ? true : false;
+    }
+
+    private boolean isAppropriateNumOfCigars(int numOfCigars) {
+        return numOfCigars >= minCigarsForParty && numOfCigars <= maxCigarsForParty;
     }
 }
